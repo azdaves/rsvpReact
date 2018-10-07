8f815114edafef922bf4bab19cc3ec320e97a584
“azdaves” <azdaveschilling@yahoo.com>
2018-10-06 20:37:21 -0700

    updated the boilerplate to my design

---
+++ src/GuestList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuestList = props => 
+        <ul>
+         {props.guests.map((guest, index) => 
+          <li>
+                <span>Joel</span>
+                <label>
+                <input type="checkbox" /> Confirmed
+                </label>
+                <button>edit</button>
+                <button>remove</button>
+          </li> 
+          )}
+        </ul>
+
+    GuestList.PropTypes = {
+        guests: PropTypes.array.isRequired
+
+    }
+
+export default GuestList;
